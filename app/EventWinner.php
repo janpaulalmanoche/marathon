48bd71f994074dba60a8426cb459f1ce41a0e908
jan paul almanoche <janpaulalmanoche@gmail.com>
2020-01-30 11:28:10 +0800

    changes

--- app/EventWinner.php
+++ app/EventWinner.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventWinner extends Model
 {
-    //
+    public function event_cat(){
+        return $this->belongsTo('App\EventCategory','event_categories_id','id');
+
+
+    }    public function category_distance_w(){
+    return $this->belongsTo('App\CategoryDistance','category_distances_id','id');
+}
+
+    public function user_w(){
+        return $this->belongsTo('App\User','user_id','id');
+    }
+
+
+    public function event_cat_dis_fee_w(){
+        return $this->belongsTo('App\EventCategoryDistanceFee','event_category_distance_fees_id','id');
+    }
+
+
+    public function participant(){
+        return $this->belongsTo
+        ('App\EventCategoryDistanceFeeParticipant','event_category_distance_fees_id','id')
+            ->count();
+    }
+
+    public function participants(){
+        return $this->hasMany('App\EventCategoryDistanceFeeParticipant',
+            'event_category_distance_fees_id','id');
+    }
 }
--- app/Http/Controllers/EventWinnerController.php
+++ app/Http/Controllers/EventWinnerController.php
@@ -5,15 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\EventCategoryDistanceFeeParticipant;
 use App\EventWinner;
+//use App\EventWinnner;
+use App\Event;
+use Carbon\Carbon;
+use App\EventCategory;
+use App\EventCategoryDistanceFee;
 class EventWinnerController extends Controller
 {
     //
 
 
     public function send($par_id){
+        $count = EventCategoryDistanceFeeParticipant::where('participant_no',$par_id)->count();
 
-        $find = EventCategoryDistanceFeeParticipant::with('event','event_cat_dis_fee','user','distance.category')->where('participant_no',$par_id)->first();
+        $msg = 'THIS NO. IS NOT REGISTERED ON THE EVENT';
+        if($count <= 0){
 
+            return response()->json(
+                [
+                    'success' => true,
+                    'data' =>'',
+                    'place' => $msg
+                ]
+            );
+        }
+
+        $find = EventCategoryDistanceFeeParticipant::with('event','event_cat_dis_fee','user','distance.category')
+            ->where('participant_no',$par_id)->first();
+
+        $place = EventWinner::
+            where('category_distances_id',$find->category_distances_id)
+            ->where('event_id',$find->event_id)
+            ->count();
+
+//        return response($find);
         $store = new EventWinner;
         $store->event_category_distance_fees_id = $find->event_category_distance_fees_id;
         $store->category_distances_id = $find->category_distances_id;
@@ -23,19 +48,50 @@ class EventWinnerController extends Controller
         $store->user_id =  $find->user_id;
         $store->save();
 
+        $total = $place + 1;
+        $msg = 'Congratulations! Rank No.'. $total;
+
         return response()->json([
            'success' => true,
-            'data' => $find
+            'data' => $find,
+            'place' => $msg
         ]);
 
 
     }
 
 
-    public function result(){
+//    public function result(){
+//
+//            $event = EventWinner::with()
+//
+//        }
 
-            $event = EventWinner::with()
+        public function event_today(){
 
+            $event = Event::whereDate('date',Carbon::now())->get();
+            return response($event);
         }
+
+        public function event_cat($event_id){
+
+                $cat = EventCategoryDistanceFee::with('event_cat','category_distance.category')
+                    ->where('event_id',$event_id)->get();
+
+            return response($cat);
+        }
+
+        public function event_cat_distance($event_cat_id){
+//                $cat = EventCategoryDistanceFee::where('')
+        }
+
+        public function get_ranking($e_c_d_f_id){
+//            $KK= App\EventWinner::orderBy('id', 'asc')->take(3)->get();
+                    $w = EventWinner::with('category_distance_w','user_w')
+                        ->where('event_category_distance_fees_id',$e_c_d_f_id)->orderBy('id','asc')->get();
+
+                    return response($w);
+        }
+
 
 }
